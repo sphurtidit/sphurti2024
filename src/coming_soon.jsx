9d3d83e8985f97a69c8086c318ac5f82b56a8309
TheDarkLord100 <miracleyt404@gmail.com>
2024-01-27 23:17:27 +0530

    Changed image path

--- src/coming_soon.jsx
+++ src/coming_soon.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./coming_soon.css";
-
+import img from "./assets/background.png";
 const Page = ()  => {
   return (
     <>
       <div className="back">
-        <img src="src/assets/background.png" alt="" />
+        <img src={img} alt="" />
       </div>
     </>
   );
