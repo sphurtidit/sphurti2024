53192c95bac851e516dfdc3ad5a819bf296eafd2
TheDarkLord100 <miracleyt404@gmail.com>
2024-01-27 22:58:08 +0530

    added coming soon page

---
+++ src/coming_soon.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./coming_soon.css";
+
+const Page = ()  => {
+  return (
+    <>
+      <div className="back">
+        <img src="src/assets/background.png" alt="" />
+      </div>
+    </>
+  );
+};
+
+export default Page;
