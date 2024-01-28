e83399fd5ad0c3350ef7fa7e0c81dbbf1df82b24
TheDarkLord100 <miracleyt404@gmail.com>
2024-01-28 22:49:17 +0530

    Correction

--- src/coming_soon.jsx
+++ src/coming_soon.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "./coming_soon.css";
-
+import backgroundImage from "./assets/back.png";
 import ImageContainer from "./Components/Image_container/ImageContainer";
 import Navbar from "./Components/LogoNavbar/LogoNavbar";
 const Page = ()  => {
+
+  const sty = {
+    backgroundImage: `url(${backgroundImage})`,
+    width: '100vw',
+    height: '100vh',
+    backgroundSize: 'cover',
+    maxWidth: '100vw',
+  };
+  
   return (
     <>
-      <div className="back">
+      <div className="back" style={sty}>
             <Navbar/>
-            <ImageContainer/>
+            <ImageContainer/>   
       </div>
     </>
   );
