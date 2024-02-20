fd3d614f68bd4a5aafd0635afb5c91fc4cedc791
Divik Goel <divikgoel75@gmail.com>
2024-02-20 18:04:31 +0530

    Messages done

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,32 @@
-<<<<<<< HEAD
-import Messages from "./Components/Messages/Messages";
+ import Messages from "./Components/Messages/Messages";
 import "./App.css";
 import SportsSection from "./Components/Sports_section/Sports_section";
 import HeroPage from "./pages/HeroPage/HeroPage";
 import Navbar from "./Components/Navbar/navbar";
 import Timer from "./Components/timer/timer";
 import TeamSec from "./Components/team_sec/team_sec";
-=======
 //  import Page from './coming_soon'
 import './App.css'
 // import SportsSection from './Components/Sports_section/Sports_section'
-import HeroPage from './pages/HeroPage/HeroPage'
-import Navbar from './Components/Navbar/navbar'
-import Timer from './Components/timer/timer'
-import TeamSec from './Components/team_sec/team_sec'
->>>>>>> 3c3c5ab8cf3f3f425cee495693c62200d29271ca
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <div className="background-container">
         <Navbar />
         <HeroPage />
         <Timer />
+        <Messages />
         <SportsSection />
         <TeamSec />
-        <Messages />
       </div>
-=======
     <div className="background-container">
     <Navbar />   
     <HeroPage />
     <Timer/>
+    <Messages />
     <SportsSection/>
     <TeamSec/>
     </div> 
->>>>>>> 3c3c5ab8cf3f3f425cee495693c62200d29271ca
     </>
   );
 }
