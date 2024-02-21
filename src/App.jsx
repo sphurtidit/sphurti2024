192f636f7a656cfe7485628321fd0312666db35b
RudrakshGupta0413 <rudrakshgupta40@gmail.com>
2024-02-21 13:11:10 +0530

    Responsiveness Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import './App.css'
 import SportsSection from './Components/Sports_section/Sports_section'
-import HeroPage from './pages/HeroPage/HeroPage'
+import HeroPage from './Components/HeroPage/HeroPage'
 import Navbar from './Components/Navbar/navbar'
 import Timer from './Components/timer/timer'
 import TeamSec from './Components/team_sec/team_sec'
 import Footer from './Components/Footer/Footer'
 import Messages from "./Components/Messages/Messages"
 import Mobile_HeroPage from './Components/Mobile_HeroPage/Mobile_HeroPage'
+import Main_HeroPage from './Components/Main_HeroPage/Main_HeroPage'
 function App() {
 
   return (
     <>
     <div className="background-container">
     <Navbar />   
-    <HeroPage />
+    <Main_HeroPage/>
+    {/* <HeroPage /> */}
     <Timer/>
     <Messages/>
     <SportsSection/>
