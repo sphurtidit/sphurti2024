77b6b936881df9fe18008f66c9f5b2effcac19ce
RudrakshGupta0413 <rudrakshgupta40@gmail.com>
2024-02-21 11:18:55 +0530

    Hero Page Mobile Design Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import HeroPage from './pages/HeroPage/HeroPage'
 import Navbar from './Components/Navbar/navbar'
 import Timer from './Components/timer/timer'
 import TeamSec from './Components/team_sec/team_sec'
+import Mobile_HeroPage from './Components/Mobile_HeroPage/Mobile_HeroPage'
 function App() {
 
   return (
     <>
     <div className="background-container">
-    <Navbar />   
-    <HeroPage />
-    <Timer/>
-    <SportsSection/>
-    <TeamSec/>
+     <Navbar />   
+    {/* <HeroPage />  */}
+    <Mobile_HeroPage />
+    {/* <Timer/> */}
+    {/* <SportsSection/> */}
+    {/* <TeamSec/> */}
     </div> 
     </>
   )
