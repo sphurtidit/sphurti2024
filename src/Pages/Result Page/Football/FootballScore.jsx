56ac98082e52b7f04e144d02f3682cb953a44a26
Harsheet Sharma <asarma626@gmail.com>
2024-03-11 16:17:14 +0530

    Fixed Bhang Bhosda

--- src/Pages/Result Page/Football/FootballScore.jsx
+++ src/Pages/Result Page/Football/FootballScore.jsx
@@ -1,32 +1,57 @@
-import './FootballScore.css'
+import "./FootballScore.css";
+import { GoDotFill } from "react-icons/go";
 
-const FootballScore=()=>{
-    return (
+const FootballScore = ({item}) => {
+  var goal1=Object.keys(item.goalTeam1)
+  var goal2=Object.keys(item.goalTeam2)
+  // console.log(item.goalTeam1[goal1[0]])
+  
+  return (
     <>
-    <div className="basketballScore">
-        <div className="teamnames">Harsheet</div>
+      <div className="footballScore">
+        <div className="teamnames">{item.team1}</div>
         <div className="ScoreInformation">
           <div className="Score1">
-            <p className="ScoreofScore">9</p>
+            <p className="ScoreofScore">{goal1.length}</p>
           </div>
           <div className="Score2">
-            <p className="ScoreofScore">2</p>
+            <p className="ScoreofScore">{goal2.length}</p>
           </div>
         </div>
-        <div className="teamnames">Vijayant</div>
+        <div className="teamnames">{item.team2}</div>
       </div>
-      <div className="tableContainer">
-        
+      <div className="tableContainer"></div>
+      <div className="table">
+        <div className="tableColumn">
+          <ul>
+            
+            {goal1.map((key)=>{
+              return(
+              <li key={key}>
+              <div>
+                <span>{item.goalTeam1[key]}</span>
+                <span className="redgoal">{key}</span>
+              </div>
+            </li>)
+            })}
+          </ul>
+        </div>
+        <div className="tableColumn">
+          <ul>
+          {goal2.map((key)=>{
+              return(
+              <li key={key}>
+              <div>
+                <span>{item.goalTeam2[key]}</span>
+                <span className="redgoal">{key}</span>
+              </div>
+            </li>)
+            })}
+          </ul>
+        </div>
       </div>
-      <table className="table">
-            <thead>
-                <tr>
-                    Player Name
-                </tr>
-                <tr>Goal time</tr>
-            </thead>
-      </table>
-    </>)
-}
+    </>
+  );
+};
 
 export default FootballScore;
--- src/Pages/Result Page/Football/FootballSection.jsx
+++ src/Pages/Result Page/Football/FootballSection.jsx
@@ -1,4 +1,54 @@
-const Football=()=>{
-    return(<></>)
-}
+import './FootballSection.css';
+import FootballScore from './FootballScore';
+import { useState, useEffect } from 'react';
+import { db } from '../../../firebase';
+import { collection, onSnapshot, query, orderBy } from 'firebase/firestore';
+
+
+const Football = () => {
+    // console.log("hi")
+    const [fixture, setFixture] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const q = query(collection(db, 'fixtures', 'Footbatt', 'boys'));
+        // console.log(q)
+        const unsub = onSnapshot(q, snapshot => {
+            const newFix = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            // console.log("hi")
+            setFixture(newFix);
+            // console.log(fixture)
+            // setLoading(false);
+            
+        });
+        return () => unsub();
+    }, []);
+    useEffect(()=>{
+        if(fixture.length>0){
+            setLoading(false)
+        }
+        else{
+            setLoading(true)
+        }
+    },[fixture])
+    
+    
+
+    return (
+        <div>
+            {loading ? <h1>Loading...</h1> :
+                <>
+                    {fixture.map((item, i) => {
+                        console.log("HI")
+                        return item.start ? 
+                            <FootballScore key={item.id} item={item}/>
+                         : <> </>
+                    }
+                    )}
+                </>
+            }
+        </div>
+    );
+};
+
 export default Football;
